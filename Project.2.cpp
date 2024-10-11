ee1154464d81290c9641949bfcdbe3b982c761b3
Patrick M <psmaloon@outlook.com>
2024-10-10 20:21:39 -0400

    Add project files.

---
+++ Project.2.cpp
@@ -0,0 +1,42 @@
+
+# include <iostream>
+using namespace std;
+
+int main() {
+
+    char op;
+    float num1, num2;
+
+    cout << "Enter operation: +, -, *, /: ";
+    cin >> op;
+
+    cout << "Enter first number : ";
+    cin >> num1;
+    cout << "Enter second number : ";
+    cin >> num2;
+
+    switch (op) {
+
+    case '+':
+        cout << num1 << " + " << num2 << " = " << num1 + num2;
+        break;
+
+    case '-':
+        cout << num1 << " - " << num2 << " = " << num1 - num2;
+        break;
+
+    case '*':
+        cout << num1 << " * " << num2 << " = " << num1 * num2;
+        break;
+
+    case '/':
+        cout << num1 << " / " << num2 << " = " << num1 / num2;
+        break;
+
+    default:
+        // If the operator is other than +, -, * or /, error message is shown
+        cout << "Error! operator is not correct";
+        break;
+    }
+    return 0;
+}
